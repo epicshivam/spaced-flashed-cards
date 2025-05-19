a2f43de00d970c57dc9e2104d532598dcf9b2ec3
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-05-19 11:20:11 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import DeckSelection from "./pages/DeckSelection";
 import FlashCardGame from "./pages/FlashCardGame";
-import Stats from "./pages/Stats";
 
 const App = () => {
   return (
@@ -14,7 +12,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/select" element={<DeckSelection />} />
         <Route path="/play/:deckId" element={<FlashCardGame />} />
-        <Route path="/stats" element={<Stats />} />
       </Routes>
     </Router>
   );
