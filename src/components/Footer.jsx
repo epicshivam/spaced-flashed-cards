96c534e6ccd3dcf16addbedb0e1a2e6e1fbe03c4
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-05-19 11:31:51 +0530

    Finished Part 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-white text-center py-2 text-sm text-gray-600">
       Made with <span className="text-red-500">❤️</span> by{" "}
       <a
-        href="#"
+        href="https://github.com/epicshivam"
         className="text-blue-500 hover:underline"
         target="_blank"
         rel="noopener noreferrer"
